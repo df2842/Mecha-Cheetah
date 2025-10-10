1519b6908f5799d1295f6a294a61d86d66d9c54b
Darius Fan <df2842@columbia.edu>
2025-10-10 18:59:36 -0400

    Leg Object Implementation

---
+++ leg.py
@@ -0,0 +1,82 @@
+from pylx16a.lx16a import *
+
+from constants import *
+
+class Leg:
+    def __init__(self, hip_servo_id=-1, knee_servo_id=-1, inverted=False):
+        if not isinstance(inverted, bool): inverted = False
+
+        self._hip = None
+        self._knee = None
+        self._inverted = inverted
+        self._running = False
+
+        LX16A.initialize("COM3")
+
+        try:
+            self._hip = LX16A(hip_servo_id)
+            self._knee = LX16A(knee_servo_id)
+
+            self._running = True
+
+        except Exception as e:
+            self._hip = None
+            self._knee = None
+
+    def move_hip(self, servo_position=0, speed=1.0):
+        if (not self._running or
+                not isinstance(servo_position, (int, float)) or
+                not isinstance(speed, (int, float))):
+            return
+
+        speed = max(0.0, min(speed, 1.0))
+        if speed == 0: return
+
+        if self._inverted: servo_position *= -1
+        servo_position += SERVO_ZERO_POSITION
+        servo_position = max(HIP_SERVO_MIN_POSITION, min(servo_position, HIP_SERVO_MAX_POSITION))
+
+        while abs(self._hip.get_physical_angle() - self._hip.get_commanded_angle()) > SERVO_TOLERANCE:
+            continue
+
+        rotation_duration = int(min(30.0, abs(servo_position - self._hip.get_physical_angle()) * SERVO_INVERSE_MAX_SPEED / speed) * 1000.0)
+
+        self._hip.move(angle=servo_position, time=rotation_duration)
+
+    def move_knee(self, servo_position=0, speed=1.0):
+        if (not self._running or
+                not isinstance(servo_position, (int, float)) or
+                not isinstance(speed, (int, float))):
+            return
+
+        speed = max(0.0, min(speed, 1.0))
+        if speed == 0: return
+
+        if self._inverted: servo_position *= -1
+        servo_position += SERVO_ZERO_POSITION
+        servo_position = max(KNEE_SERVO_MIN_POSITION, min(servo_position, KNEE_SERVO_MAX_POSITION))
+
+        while abs(self._knee.get_physical_angle() - self._knee.get_commanded_angle()) > SERVO_TOLERANCE:
+            continue
+
+        rotation_duration = int(min(30.0, abs(servo_position - self._knee.get_physical_angle()) * SERVO_INVERSE_MAX_SPEED / speed) * 1000.0)
+
+        self._knee.move(angle=servo_position, time=rotation_duration)
+
+    def move_all(self, hip_servo_position=0, knee_servo_position=0, hip_speed=1.0, knee_speed=1.0):
+        while (abs(self._hip.get_physical_angle() - self._hip.get_commanded_angle()) > SERVO_TOLERANCE or
+               abs(self._knee.get_physical_angle() - self._knee.get_commanded_angle()) > SERVO_TOLERANCE):
+            continue
+
+        self.move_hip(servo_position=hip_servo_position, speed=hip_speed)
+        self.move_knee(servo_position=knee_servo_position, speed=knee_speed)
+
+    def stop_hip(self):
+        self._hip.move_stop()
+
+    def stop_knee(self):
+        self._hip.move_stop()
+
+    def stop_all(self):
+        self._hip.move_stop()
+        self._knee.move_stop()
---
+++ main.py
@@ -0,0 +1,8 @@
+from constants import *
+from leg import Leg
+
+if __name__ == "__main__":
+    fl_leg = Leg(hip_servo_id=FL_HIP_SERVO_ID, knee_servo_id=FL_KNEE_SERVO_ID, inverted=False)
+
+    fl_leg.move_all(hip_servo_position=50, knee_servo_position=-50, hip_speed=0.5, knee_speed=1)
+    fl_leg.move_all(hip_servo_position=-50, knee_servo_position=50, hip_speed=0.5, knee_speed=1)
