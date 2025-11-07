59042f92318f2ee15ee36cd4d0a57e37759165dd
Darius Fan <df2842@columbia.edu>
2025-11-07 16:25:03 -0500

    Tweaked electronics-dependant constants

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from chassis import Chassis
 from leg import Leg
 
 if __name__ == "__main__":
-    LX16A.initialize("COM3")
+    LX16A.initialize("/dev/ttyUSB0")
 
     chassis = Chassis(fl_hip_servo_id=FL_HIP_SERVO_ID, fl_knee_servo_id=FL_KNEE_SERVO_ID, fr_hip_servo_id=FR_HIP_SERVO_ID, fr_knee_servo_id=FR_KNEE_SERVO_ID,
                       bl_hip_servo_id=BL_HIP_SERVO_ID, bl_knee_servo_id=BL_KNEE_SERVO_ID, br_hip_servo_id=BR_HIP_SERVO_ID, br_knee_servo_id=BR_KNEE_SERVO_ID)
